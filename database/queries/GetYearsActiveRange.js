1b14094fd31043672fc69e6eb029022fac586263
ravindra rawat <ravinrawat.wd@gmail.com>
2018-08-29 12:42:49 -0600

    search artist query completed

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds the lowest and highest yearsActive of artists in the Artist collection
@@ -6,4 +6,14 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  Artist.find({})
+    .sort({ yearsActive: 1 })
+    .then(users => {
+      let min = users[0].yearsActive;
+      let max = users[users.length - 1].yearsActive;
+      return {
+        min: min,
+        max: max
+      };
+    });
 };
